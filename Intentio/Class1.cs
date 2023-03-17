f6025482f8ce0793e4941d5712c2b0615d6dd70f
mhamido <mahmoudmhamido@gmail.com>
2023-03-17 23:55:17 +0200

    Add project files.

---
+++ Intentio/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intentio
+{
+    abstract class IUser
+    {
+        public virtual string ID { get; set; }
+        public virtual string Name { get; set; }
+    }
+
+    sealed class Child : IUser
+    {
+    }
+
+    sealed class Parent : IUser 
+    {
+    }
+}
---
+++ Intentio/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Intentio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
