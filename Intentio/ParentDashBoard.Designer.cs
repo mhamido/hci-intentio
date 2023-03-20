b65ccd63acc196ab932ed67ee68ee64a5f2da9e6
lolaomar <alyaa.omar@msa.edu.eg>
2023-03-20 22:39:09 +0200

    table

--- Intentio/ParentDashBoard.Designer.cs
+++ Intentio/ParentDashBoard.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TimesDistracted = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TimeToComplete = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LettersMistaken = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NumbersMistaken = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TotalMistakes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Number,
+            this.Name,
+            this.TimesDistracted,
+            this.TimeToComplete,
+            this.LettersMistaken,
+            this.NumbersMistaken,
+            this.TotalMistakes});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 266);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1005, 306);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Number
+            // 
+            this.Number.HeaderText = "Number";
+            this.Number.Name = "Number";
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            // 
+            // TimesDistracted
+            // 
+            this.TimesDistracted.HeaderText = "TimesDistracted";
+            this.TimesDistracted.Name = "TimesDistracted";
+            // 
+            // TimeToComplete
+            // 
+            this.TimeToComplete.HeaderText = "TimeToComplete";
+            this.TimeToComplete.Name = "TimeToComplete";
+            // 
+            // LettersMistaken
+            // 
+            this.LettersMistaken.HeaderText = "LettersMistaken";
+            this.LettersMistaken.Name = "LettersMistaken";
+            // 
+            // NumbersMistaken
+            // 
+            this.NumbersMistaken.HeaderText = "NumbersMistaken";
+            this.NumbersMistaken.Name = "NumbersMistaken";
+            // 
+            // TotalMistakes
+            // 
+            this.TotalMistakes.HeaderText = "TotalMistakes";
+            this.TotalMistakes.Name = "TotalMistakes";
+            // 
+            // ParentDashBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1029, 584);
+            this.Controls.Add(this.dataGridView1);
+            this.Name.Name = "ParentDashBoard";
+            this.Name.HeaderText = "ParentDashBoard";
             this.Text = "ParentDashBoard";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TimesDistracted;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TimeToComplete;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LettersMistaken;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NumbersMistaken;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TotalMistakes;
     }
 }
